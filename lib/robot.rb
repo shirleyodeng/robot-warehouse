222f3db3299239c44c97b0f93c02c3b48e8647b7
Shirley Thompson <shirleyodeng@gmail.com>
2024-01-25 22:44:44 +0000

    feat: add Robot class

---
+++ lib/robot.rb
@@ -0,0 +1,47 @@
+class Robot
+  class InvalidCommandError < StandardError; end
+
+  class OutsideWarehouseError < StandardError; end
+
+  VALID_COMMANDS = %w[N E S W].freeze
+
+  attr_reader :final_coordinates, :starting_coordinates
+
+  def initialize(starting_coordinates = [0, 0])
+    validate_coordinates!(starting_coordinates)
+
+    @final_coordinates = starting_coordinates.dup
+    @starting_coordinates = starting_coordinates
+  end
+
+  def move!(command)
+    validate_command!(command)
+
+    case command
+    when "N"
+      final_coordinates[1] -= 1
+    when "E"
+      final_coordinates[0] += 1
+    when "S"
+      final_coordinates[1] += 1
+    when "W"
+      final_coordinates[0] -= 1
+    end
+
+    validate_coordinates!(final_coordinates)
+  end
+
+  private
+
+  def validate_command!(command)
+    return true if VALID_COMMANDS.include?(command)
+
+    raise InvalidCommandError
+  end
+
+  def validate_coordinates!(coordinates)
+    return true if coordinates.all? { |coordinate| coordinate.between?(0, 9) }
+
+    raise OutsideWarehouseError
+  end
+end
---
+++ spec/robot_spec.rb
@@ -0,0 +1,72 @@
+require 'robot'
+
+RSpec.describe Robot do
+  subject { described_class.new(starting_coordinates) }
+
+  let(:starting_coordinates) { [5, 5] }
+
+  describe "#move" do
+    context "when the command is N" do
+      let(:command) { "N" }
+
+      it "moves the robot north" do
+        expect { subject.move(command) }
+          .to change { subject.final_coordinates }
+          .from(starting_coordinates)
+          .to([5, 4])
+      end
+    end
+
+    context "when the command is E" do
+      let(:command) { "E" }
+
+      it "moves the robot east" do
+        expect { subject.move(command) }
+          .to change { subject.final_coordinates }
+          .from(starting_coordinates)
+          .to([6, 5])
+      end
+    end
+
+    context "when the command is S" do
+      let(:command) { "S" }
+
+      it "moves the robot south" do
+        expect { subject.move(command) }
+          .to change { subject.final_coordinates }
+          .from(starting_coordinates)
+          .to([5, 6])
+      end
+    end
+
+    context "when the command is W" do
+      let(:command) { "W" }
+
+      it "moves the robot west" do
+        expect { subject.move(command) }
+          .to change { subject.final_coordinates }
+          .from(starting_coordinates)
+          .to([4, 5])
+      end
+    end
+
+    context "when the command is invalid" do
+      let(:command) { "X" }
+
+      it "raises an InvalidCommand error" do
+        expect { subject.move(command) }
+          .to raise_error(described_class::InvalidCommandError)
+      end
+    end
+
+    context "when the command moves the robot outside the warehouse" do
+      let(:command) { "N" }
+      let(:starting_coordinates) { [0, 0] }
+
+      it "raises an OutsideWarehouse error" do
+        expect { subject.move(command) }
+          .to raise_error(described_class::OutsideWarehouseError)
+      end
+    end
+  end
+end
